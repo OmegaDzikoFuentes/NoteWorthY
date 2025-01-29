139b274570639e9c21ef8fc0e1b44ac21774721a
kyle <averytkyle@gmail.com>
2025-01-28 21:06:26 -0500

    added notes migration, seeder, model

---
+++ app/models/notes.py
@@ -0,0 +1,20 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class Notes(db.Model):
+    __tablename__ = 'notes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
+    notebook_id = db.Column(db.Integer, nullable=False)
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'notebook_id': self.notebook_id
+        }
