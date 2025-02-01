65c2fc0abf269105cbf584b80d6473cf84bd64b5
MarkSabangan <marksabangan1217@gmail.com>
2025-01-31 18:15:00 -0800

    fixed notetag id for Fkeys

--- app/models/notetag.py
+++ app/models/notetag.py
@@ -10,7 +10,7 @@ class NoteTag(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    note_id = db.Column(db.Integer, db.ForeignKey("notes.id"), nullable=False)
-    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), nullable=False)
+    note_id = db.Column(db.Integer, db.ForeignKey(Notes.id), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey(Tag.id), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=func.now())
     updated_at = db.Column(db.DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
