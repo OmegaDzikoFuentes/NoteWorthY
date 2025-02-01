bbe6cd701dcf62d1547fcd8ab85e65202897f525
Aaron Vargas <vargasaaron62@gmail.com>
2025-01-31 17:13:51 -0800

    fixed tasks model and seeder

--- app/seeds/tasks.py
+++ app/seeds/tasks.py
@@ -1,15 +1,16 @@
 from app.models import db, Task, environment, SCHEMA
 from sqlalchemy.sql import text
+from datetime import datetime
 
 def seed_tasks():
     demo_task = Task(
-        title = 'Demo Task 2', description = 'Finish rough draft for essay', due_date = '12/01/2025', completed = False, notebook_id = 1
+        title = 'Demo Task 2', description = 'Finish rough draft for essay',  due_date=datetime.strptime("12/01/2025", "%m/%d/%Y").date(), completed = False, notebook_id = 1
     )
     demo_task2 = Task(
-        title = 'Demo Task 3', description = 'Finish cover art for essay', due_date = '12/01/2025', completed = False, notebook_id = 1
+        title = 'Demo Task 3', description = 'Finish cover art for essay',  due_date=datetime.strptime("12/01/2025", "%m/%d/%Y").date(), completed = False, notebook_id = 1
     )
     demo_task3 = Task(
-        title = 'Demo Task 4', description = 'Finish letter of thanks for essay', due_date = '12/01/2025', completed = False, notebook_id = 1
+        title = 'Demo Task 4', description = 'Finish letter of thanks for essay',  due_date=datetime.strptime("12/01/2025", "%m/%d/%Y").date(), completed = False, notebook_id = 1
     )
 
     db.session.add(demo_task)
@@ -17,8 +18,8 @@ def seed_tasks():
     db.session.add(demo_task3)
     db.session.commit()
 
-    def undo_tasks():
-        if enviroment == 'production':
+def undo_tasks():
+        if environment == 'production':
             db.session.execute(f"TRUNCATE table {SCHEMA}.tasks RESTART IDENTITY CASCADE;")
         else:
             db.session.execute(text("DELETE FROM tasks"))
