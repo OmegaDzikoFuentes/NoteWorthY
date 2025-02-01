82e1e1dc4cd5b0cce46760ca212ea6adc78e54fa
MarkSabangan <marksabangan1217@gmail.com>
2025-01-31 18:18:56 -0800

    fixed relationship for notetags

--- app/models/tags.py
+++ app/models/tags.py
@@ -13,7 +13,7 @@ class Tag(db.Model):
     updated_at = db.Column(db.DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
 
     # Relationship
-    notes = db.relationship("Note", secondary="notetags", back_populates="tags")
+    notes = db.relationship("notes", secondary="notetags", back_populates="tags")
 
     def to_dict(self):
         return {
