68ec2d2e93fba2ceecb3c2758a35948b514dd7ad
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-01 21:11:23 -0800

    delete a task and its error handling have been implemented

--- app/api/tasks_routes.py
+++ app/api/tasks_routes.py
@@ -35,3 +35,22 @@ def new_task():
     db.session.commit()
 
     return task.to_dict()
+
+# Delete a task
+@tasks_routes.route('/<int:task_id>', methods=['DELETE'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return jsonify({"message": "Task not found"}), 404
+
+    notebook = Notebook.query.get(task.notebook_id)
+
+    if notebook.user_id != current_user.id:
+        return jsonify({"message": "Unauthorized"}), 401
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({"message": "Task has been deleted successfully"})
