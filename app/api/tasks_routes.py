dc435c45bf6c1b961da5b1002a6118c5e992f54a
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-01 21:00:33 -0800

    create a new task route working, consider dating format later

--- app/api/tasks_routes.py
+++ app/api/tasks_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Task, Notebook
 from flask_login import current_user, login_required
+from app.models.db import db
+from datetime import datetime
 
 tasks_routes = Blueprint('tasks', __name__)
 
@@ -16,3 +18,20 @@ def current_tasks():
     return jsonify([task.to_dict() for task in tasks])
 
 # create a new task
+@tasks_routes.route('/new', methods=['POST'])
+@login_required
+def new_task():
+    data = request.json
+
+    task = Task(
+        title=data['title'],
+        description=data['description'],
+        due_date=datetime.strptime(data['due_date'], "%m/%d/%Y").date(),
+        completed=data['completed'],
+        notebook_id=data['notebook_id']
+    )
+
+    db.session.add(task)
+    db.session.commit()
+
+    return task.to_dict()
