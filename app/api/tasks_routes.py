a1a4ec120d3e0df8761a6a29da2b44066dc6403b
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-03 15:05:09 -0800

    finished update task route and added proper dynamic creation

--- app/api/tasks_routes.py
+++ app/api/tasks_routes.py
@@ -17,30 +17,39 @@ def current_tasks():
 
     return jsonify([task.to_dict() for task in tasks])
 
-# create a new task
+# Create a new task
 @tasks_routes.route('/new', methods=['POST'])
 @login_required
 def new_task():
     data = request.json
 
-    if not data or 'title' not in data or data['title'] is None or data['title'] == '':
+    if not data or not data.get('title'):
         return jsonify({"message": "Title is required"}), 400
 
-    task = Task(
-        title=data['title'],
-        description=data['description'],
-        due_date=datetime.strptime(data['due_date'], "%m/%d/%Y").date(),
-        completed=data['completed'],
-        notebook_id=data['notebook_id']
-    )
+    if not data or not data.get('notebook_id'):
+        return jsonify({"message": "Notebook id is required"}), 400
 
-    if task is None:
-        return jsonify({"message": "Invalid task"}), 404
+    task_data = {
+        'title': data['title'],
+        'notebook_id': data['notebook_id'],
+    }
+
+    if 'description' in data:
+        task_data['description'] = data['description']
+
+    if 'completed' in data:
+        task_data['completed'] = data['completed']
+
+    if 'due_date' in data:
+         task_data["due_date"] = datetime.strptime(data["due_date"], "%m/%d/%Y").date()
+
+    task = Task(**task_data)
 
     db.session.add(task)
     db.session.commit()
 
-    return task.to_dict()
+    return jsonify(task.to_dict())
+
 
 # Delete a task
 @tasks_routes.route('/<int:task_id>', methods=['DELETE'])
@@ -60,3 +69,41 @@ def delete_task(task_id):
     db.session.commit()
 
     return jsonify({"message": "Task has been deleted successfully"})
+
+# Update a task
+@tasks_routes.route('<int:task_id>', methods=['PUT'])
+@login_required
+def update_task(task_id):
+    task = Task.query.get(task_id)
+
+    if task is None:
+        return jsonify({"message": "Task not found"}), 404
+
+    notebook = Notebook.query.get(task.notebook_id)
+
+    if notebook.user_id != current_user.id:
+        return jsonify({"message": "Unauthorized"}), 401
+
+    data = request.json
+
+    if not data:
+        return jsonify({"message": "Invalid request"}), 400
+
+    if 'title' in data:
+        task.title = data['title']
+
+    if 'description' in data:
+        task.description = data['description']
+
+    if 'due_date' in data:
+        task.due_date = data['due_date']
+
+    if 'completed' in data:
+        task.completed = data['completed']
+
+    if 'due_date' in data:
+        task.due_date = datetime.strptime(data['due_date'], "%m/%d/%Y").date() if data['due_date'] else None
+
+    db.session.commit()
+
+    return jsonify(task.to_dict())
