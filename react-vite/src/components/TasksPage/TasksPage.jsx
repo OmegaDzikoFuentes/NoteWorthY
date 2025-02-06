d767579bd78c75b8b3c9216e6f3f3357853f1643
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-05 20:58:08 -0800

    created tasks component and added to router

---
+++ react-vite/src/components/TasksPage/TasksPage.jsx
@@ -0,0 +1,12 @@
+import "./TasksPage.css";
+
+function TasksPage() {
+  return (
+    <div>
+      <h1>You are on the tasks page!</h1>
+      <h2>Good job</h2>
+    </div>
+  );
+}
+
+export default TasksPage;
