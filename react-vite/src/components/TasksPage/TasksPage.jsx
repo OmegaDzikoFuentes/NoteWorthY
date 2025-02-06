ef22066adddad2de6bc7504ab647767cadc5ec66
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-05 22:00:56 -0800

    basic styling and layout for tasks page

--- react-vite/src/components/TasksPage/TasksPage.jsx
+++ react-vite/src/components/TasksPage/TasksPage.jsx
@@ -2,9 +2,27 @@ import "./TasksPage.css";
 
 function TasksPage() {
   return (
-    <div>
-      <h1>You are on the tasks page!</h1>
-      <h2>Good job</h2>
+    <div className="tasks-page-container">
+      <div className="tasks-header-box">
+        <h1 className="tasks-header">Tasks</h1>
+      </div>
+      <div className="tasks-search-container">
+        <h4 className="task-count">(Number) task(s)</h4>
+        <div className="new-task-search">
+          <button className="tasks-page-new">New Task</button>
+          <input
+            className="tasks-page-search"
+            type="text"
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+      <div className="tasks-list-container">
+        <div className="tasks-list-headers">
+          <p className="tasks-list-header-text tasks-list-title">Title</p>
+          <p className="tasks-list-header-text tasks-list-due-date">Due Date</p>
+        </div>
+      </div>
     </div>
   );
 }
