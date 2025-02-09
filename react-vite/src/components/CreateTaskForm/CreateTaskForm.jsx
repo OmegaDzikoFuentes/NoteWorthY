a09a0cc70c98886fdf62193932177b68741bbfce
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-08 21:18:32 -0800

    misc

--- react-vite/src/components/CreateTaskForm/CreateTaskForm.jsx
+++ react-vite/src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -34,39 +34,46 @@ function CreateTaskForm() {
   }, []);
 
   return (
-    <div className="create-task-form-container">
-      <h2 className="create-task-header">Create a New Task</h2>
-      <form className="create-task-form">
-        <label htmlFor="notebooks" className="input-label notebook-select">
-          Notebook:
-          <select name="notebooks" id="notebooks">
-            {notebooks.map((notebook) => (
-              <option key={notebook.id} value={notebook.id}>
-                {notebook.name}
-              </option>
-            ))}
-          </select>
-        </label>
+    <>
+      {isLoaded && (
+        <div className="create-task-form-container">
+          <h2 className="create-task-header">Create a New Task</h2>
+          <form className="create-task-form">
+            <label htmlFor="notebooks" className="input-label notebook-select">
+              Notebook:
+              <select name="notebooks" id="notebooks">
+                {notebooks.map((notebook) => (
+                  <option key={notebook.id} value={notebook.id}>
+                    {notebook.name}
+                  </option>
+                ))}
+              </select>
+            </label>
 
-        <label className="input-label title-input">
-          Title:
-          <input
-            className="create-task-input"
-            type="text"
-            placeholder="Title..."
-          />
-        </label>
-        <label className="input-label">
-          Description:
-          <textarea className="create-task-ta" placeholder="Description..." />
-        </label>
-        <label className="input-label due-date-input">
-          Due Date:
-          <input type="date" />
-        </label>
-        <button className="create-task-btn">Create Task</button>
-      </form>
-    </div>
+            <label className="input-label title-input">
+              Title:
+              <input
+                className="create-task-input"
+                type="text"
+                placeholder="Title..."
+              />
+            </label>
+            <label className="input-label">
+              Description:
+              <textarea
+                className="create-task-ta"
+                placeholder="Description..."
+              />
+            </label>
+            <label className="input-label due-date-input">
+              Due Date:
+              <input type="date" />
+            </label>
+            <button className="create-task-btn">Create Task</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
