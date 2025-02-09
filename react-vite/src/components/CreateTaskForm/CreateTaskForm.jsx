b843c19b23cef08909634cf7a114269cd8686c4d
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-08 20:46:37 -0800

    fixed css overreach

--- react-vite/src/components/CreateTaskForm/CreateTaskForm.jsx
+++ react-vite/src/components/CreateTaskForm/CreateTaskForm.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import "./CreateTaskForm.css";
 
 function CreateTaskForm() {
@@ -8,16 +7,24 @@ function CreateTaskForm() {
       <form className="create-task-form">
         <label htmlFor="notebooks" className="input-label notebook-select">
           Notebook:
-          <select name="notebooks" id="notebooks"></select>
+          <select name="notebooks" id="notebooks">
+            <option value="notebook1">Notebook 1</option>
+            <option value="notebook2">Notebook 2</option>
+            <option value="notebook3">Notebook 3</option>
+          </select>
         </label>
 
         <label className="input-label title-input">
           Title:
-          <input type="text" placeholder="Title..." />
+          <input
+            className="create-task-input"
+            type="text"
+            placeholder="Title..."
+          />
         </label>
         <label className="input-label">
           Description:
-          <textarea placeholder="Description..." />
+          <textarea className="create-task-ta" placeholder="Description..." />
         </label>
         <label className="input-label due-date-input">
           Due Date:
