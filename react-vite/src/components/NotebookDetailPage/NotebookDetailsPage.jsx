4a7ef76b3e5778d7a43874bc5b761042a27d7f7e
Omega Dziko F. <megaoy@yahoo.com>
2025-02-11 20:35:43 -0500

    added css to notebookdetailspage

--- react-vite/src/components/NotebookDetailPage/NotebookDetailsPage.jsx
+++ react-vite/src/components/NotebookDetailPage/NotebookDetailsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getNotebookById } from "../../redux/notebook";
+import "./NotebookDetailsPage.css"
 
 const NotebookDetailsPage = () => {
   const { notebookId } = useParams();
@@ -15,10 +16,20 @@ const NotebookDetailsPage = () => {
   if (!notebook) return <p>Loading notebook...</p>;
 
   return (
-    <div className="notebook-details">
-      <h1>{notebook.name}</h1>
-      <p>Created at: {new Date(notebook.created_at).toLocaleString()}</p>
-      <p>Last updated: {new Date(notebook.updated_at).toLocaleString()}</p>
+    <div className="notebook-details-page-container">
+      <div className="notebook-details-header-box">
+        <h1 className="notebook-details-header">{notebook.name}</h1>
+      </div>
+      <div className="notebook-details-info">
+        <p className="notebook-details-text">
+          <strong>Created at:</strong>{" "}
+          {new Date(notebook.created_at).toLocaleString()}
+        </p>
+        <p className="notebook-details-text">
+          <strong>Last updated:</strong>{" "}
+          {new Date(notebook.updated_at).toLocaleString()}
+        </p>
+      </div>
     </div>
   );
 };
