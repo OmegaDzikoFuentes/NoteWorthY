5b03dd166ccb989a511c39ab9be8e9be1dfc32af
Aaron Vargas <vargasaaron62@gmail.com>
2025-02-13 17:08:42 -0800

    ready to add to notebooks page

--- react-vite/src/components/Notes/Notes.jsx
+++ react-vite/src/components/Notes/Notes.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getNotesForNotebook } from "../../redux/notes";
-import NotebookTasks from "../NotebookTasks";
 import "./Notes.css";
 
 function Notes({ notebookId }) {
