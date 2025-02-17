2279a0bc45e4a41fb67aeb8450edfe9efd0f05d9
MarkSabangan <marksabangan1217@gmail.com>
2025-02-16 17:54:06 -0800

    fixed tag filter

--- react-vite/src/components/Notes/UserNotes.jsx
+++ react-vite/src/components/Notes/UserNotes.jsx
@@ -41,13 +41,13 @@ function UserNotes() {
 
   useEffect(() => {
     if (selectedTags.size > 0) {
-      selectedTags.forEach((tag) => {
-        if (!notesByTag[tag]) dispatch(fetchNotesByTag(tag));
-      });
+        Array.from(selectedTags).forEach(tag => {
+            if (!notesByTag[tag]) dispatch(fetchNotesByTag(tag));
+        });
     } else {
-      dispatch(getCurrentUserNotes());
+        dispatch(getCurrentUserNotes());
     }
-  }, [dispatch]);
+}, [dispatch, Array.from(selectedTags).join(",")]);
 
   const displayedNotes =
     selectedTags.size > 0
